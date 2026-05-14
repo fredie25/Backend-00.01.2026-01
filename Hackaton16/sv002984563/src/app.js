cf332793b96580af6cbf56d5ec328da471f88f2f
Keterlyn <keterlyn.moreno@gmail.com>
2026-05-13 23:45:08 -0500

    Hackaton16 parte2

--- Hackaton16/sv002984563/src/app.js
+++ Hackaton16/sv002984563/src/app.js
@@ -1,8 +1,31 @@
+const path = require(`path`);
+const bodyParser = require("body-parser");
 const express = require(`express`);
+const passport = require("passport");
+const session = require(`express-session`);
 
 const app = express();
 
-
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));  
+
+const SECRET = process.env.SECRET;
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session(
+    {
+        secret: SECRET,
+        resave:false,
+        saveUninitialized:false,
+        cookie:{
+            secure:process.env.NODE_ENV === `production`,
+            maxAge:24 * 60 * 60 * 1000
+        }
+    }
+));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 module.exports = app;
--- Hackaton16/sv002984563/src/server.js
+++ Hackaton16/sv002984563/src/server.js
@@ -2,11 +2,19 @@ require (`dotenv`).config();
 console.log(`Inicio de aplicacion`);
 
 const http = require(`http`);
+const path = require(`path`);
 
 const app = require(`./app`);
-const db = require(`./config/db`);
-
 const server = http.createServer(app);
+const io = require(`socket.io`)(server);
+
+const bodyParser = require(`body-parser`);
+const passtport = require(`passport`);
+
+const bcryptjs = require(`bcryptjs`);
+
+const session = require(`express-session`);
+const db = require(`./config/db`);
 
 const PORT = process.env.PORT;
 
